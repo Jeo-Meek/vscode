0b4d85f58ada246896b75178643192adaca01853
meek <jiaxiangan@kingyee.com.cn>
2021-08-11 14:37:18 +0800

    routes_change

--- .history/routes/web_20210811142724.php
+++ .history/routes/web_20210811142724.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\UsersController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,4 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/','UsersController@root');
+Route::get('/', function () {
+    return view('welcome');
+});
--- .history/routes/web_20210811143137.php
+++ .history/routes/web_20210811143137.php
@@ -14,4 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/','UsersController@root');
+Route::get('/',UsersController@root);
